31cf7a46554ef90c5cf4b2225547ab16be326c99
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-01-28 17:16:35 -0500

    multiplication exercise refactor

--- src/main/java/czik/cs673/assignments/assignment1/MultiplicationCalculator.java
+++ src/main/java/czik/cs673/assignments/assignment1/MultiplicationCalculator.java
@@ -1,21 +1,21 @@
 package czik.cs673.assignments.assignment1;
 
-import java.util.Scanner;
-
 public class MultiplicationCalculator {
 
+    static final int MAX_BASE = 12;
     static final int MAX_MULTIPLE = 12;
 
     public static void run(String[] args) {
         System.out.println("--- Multiplication Table ---");
 
-        try (Scanner inputScanner = new Scanner(System.in)) {
-            System.out.println("Enter Base Value:");
-            Integer baseValue = inputScanner.nextInt();
-
-            System.out.println("Generating Multiplication Table:");
+        try {
             for (int i = 1; i <= MAX_MULTIPLE; i++) {
-                System.out.println(String.format("%d X %d = %d", baseValue, i, Math.multiplyExact(baseValue, i)));
+                StringBuilder builder = new StringBuilder();
+                for (int j = 1; j <= MAX_BASE; j++) {
+                    String tableEntry = String.format("%d X %d = %d", j, i, Math.multiplyExact(j, i));
+                    builder.append(String.format("%1$-15s", tableEntry));
+                }
+                System.out.println(builder.toString());
             }
         } catch (Exception e) {
             System.out.println(String.format("Exception: %s", e.getMessage()));
