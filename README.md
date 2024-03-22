791278d579cabf2c896f4c7913e61dfdc7086a45
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-03-22 11:38:05 -0400

    Root webpage setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# CS673 Assignments
+All of my homework assignments for the spring 2024 semester in one place
+
+# Assignment 4
+The two web pages for this assignment are run from the same application.
+
+## Running the app
+Run from project root
+
+#### `mvn spring-boot:run`
+Runs the app in the development mode.\
+Open [http://localhost:8080](http://localhost:8080) to view it in your browser.
+
---
+++ src/test/java/czik/cs673/assignments/assignment4/controllers/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package czik.cs673.assignments.assignment4.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SuppressWarnings("null")
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Assignment 4")));
+    }
+}
